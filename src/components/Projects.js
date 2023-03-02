0b94bdb5c584bcf592f860c87dccd7c10802124d
Kyle Joseph <kyle@Kyles-Air.hsd1.or.comcast.net>
2023-03-02 01:50:34 -0800

    Added comments and updated image

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,6 +7,7 @@ import Keshi from '.././images/keshiImage.jpg'
 import animeImage1 from '.././images/animeImage1.jpg'
 import teaImage from '.././images/teaImage.jpg'
 import loveImage from '.././images/loveImage.jpg'
+import loveImage2 from '.././images/loveImage2.jpeg'
 import triniImage from '.././images/triniImage.jpg'
 
 function Projects() {
@@ -17,7 +18,7 @@ function Projects() {
       <Work image={Keshi} altImage='Keshi' jobTitle='Personal Project' companyName='Keshi Music Bracket' workDesc='Html, CSS, JavaScript, Bootstrap' redirect='https://keshi-music-bracket.web.app/' />
       <Work image={animeImage1} altImage='anime retriever' jobTitle='Personal Project' companyName='Anime Retriever' workDesc='React, Material UI, Firebase, Fetch API, CSS' redirect='https://anime-retriever.web.app/' />
       <Work image={teaImage} altImage='tea cozy' jobTitle='Personal Project' companyName='Tea Cozy Design' workDesc='Html, CSS' redirect='https://tea-cozy-2fba6.web.app/' />
-      <Work image={loveImage} altImage='love probability generator' jobTitle='Personal Project' companyName='Love Probability Generator' workDesc='React, Router, Express, Axios, CSS' redirect='https://github.com/kylejosephswe/LoveProbabilityGenerator' />
+      <Work image={loveImage2} altImage='love probability generator' jobTitle='Personal Project' companyName='Love Probability Generator' workDesc='React, Router, Express, Axios, CSS' redirect='https://github.com/kylejosephswe/LoveProbabilityGenerator' />
       <Work image={triniImage} altImage='tasty trini' jobTitle='Personal Project' companyName='Tasty Trini!' workDesc='TypeScript, React Native, Expo, Android Studio, Context API, Native Navigation' redirect='https://github.com/kylejosephswe/Tasty-Trini' />
     </div>
   )
