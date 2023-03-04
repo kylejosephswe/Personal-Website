e27bd911a37d3a1f5fa234417726797a9c0356e4
kylejoseph46 <kylejosephswe@gmail.com>
2023-03-03 18:01:40 -0800

    Added links to company and projects names

--- src/components/Work.js
+++ src/components/Work.js
@@ -5,11 +5,12 @@ function Work({ image, altImage, jobTitle, companyName, workDesc, redirect }) {
         <div className="work-image-text-row">
           <div>
              {redirect ? <a className="project-links" href={redirect} target="_blank" rel="noreferrer" > <img className="work-img-design" width="500" height="460" src={image} alt={altImage} /> </a> : <img className="work-img-design" src={image} alt={altImage} />}
-            {/*<img className="work-img-design" width="500" height="460" src={image} alt={altImage} /> */}
           </div>
           <div className="work-text-column">
             <h3 className="job-title">{jobTitle}</h3>
+            <a className="a-link" href={redirect} target="_blank" rel="noreferrer">
             <h2 className="company-name">{companyName}</h2>
+            </a>
             <p className="work-desc">{workDesc}</p>
           </div>
         </div>
