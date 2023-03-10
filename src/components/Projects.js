84a21318efb06d6d56aadb53008f57d29a1a2bc1
kylejoseph46 <kylejosephswe@gmail.com>
2023-03-09 20:03:14 -0800

    Changed project from work-in-progress

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -13,9 +13,9 @@ function Projects() {
     <div>
       <Navbar />
       <h1 className="projects-header">Projects</h1>
+      <Work image={NikeLogo} altImage='Nike Sneaker Gallery' jobTitle='Personal Project' companyName='Nike Sneaker Gallery' workDesc='React, Router, Context API, Firebase, CSS' redirect='https://sneaker-gallery.web.app/' />
       <Work image={Keshi} altImage='Keshi' jobTitle='Personal Project' companyName='Keshi Music Bracket' workDesc='TypeScript, Html, CSS' redirect='https://keshi-music-bracket.web.app/' />
-      <Work image={NikeLogo} altImage='Nike Sneaker Gallery' jobTitle='Personal Project' companyName='Nike Sneaker Gallery (Work in Progress)' workDesc='React, Router, Context API, Firebase, CSS' redirect='https://sneaker-gallery.web.app/' />
-      <Work image={animeImage1} altImage='anime retriever' jobTitle='Personal Project' companyName='Anime Retriever' workDesc='React, Material UI, Fetch API, Firebase, CSS' redirect='https://anime-retriever.web.app/' />
+      <Work image={animeImage1} altImage='anime retriever' jobTitle='Personal Project' companyName='Anime Retriever' workDesc='React, Fetch API, Firebase, CSS' redirect='https://anime-retriever.web.app/' />
       <Work image={teaImage} altImage='tea cozy' jobTitle='Personal Project' companyName='Tea Cozy Design' workDesc='Html, CSS' redirect='https://tea-cozy-2fba6.web.app/' />
     </div>
   )
