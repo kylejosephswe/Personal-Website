9027dbe6df2037506a954f8e4f3b5e72cfdeff8f
kylejoseph46 <kylejosephswe@gmail.com>
2023-03-19 17:05:03 -0700

    Added pokeAPI to description of National Pokedex

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -17,7 +17,7 @@ function Projects() {
       <Work image={NikeLogo} altImage='Nike Sneaker Gallery' jobTitle='Personal Project' companyName='Nike Sneaker Gallery' workDesc='React, Router, Context API, Firebase, CSS' redirect='https://sneaker-gallery.web.app/' />
       <Work image={Keshi} altImage='Keshi' jobTitle='Personal Project' companyName='Keshi Music Bracket' workDesc='TypeScript, Html, Firebase, CSS' redirect='https://keshi-music-bracket.web.app/' />
       <Work image={animeImage} altImage='anime retriever' jobTitle='Personal Project' companyName='Anime Retriever' workDesc='React, Fetch API, Firebase, CSS' redirect='https://anime-retriever.web.app/' />
-      <Work image={pokemon} altImage='Pokedex' jobTitle='Personal Project' companyName='National Pokedex' workDesc='React, Firebase, CSS' redirect='https://national-pokedex.web.app/' />
+      <Work image={pokemon} altImage='Pokedex' jobTitle='Personal Project' companyName='National Pokedex' workDesc='React, PokéAPI, Firebase, CSS' redirect='https://national-pokedex.web.app/' />
       <Work image={teaImage} altImage='tea cozy' jobTitle='Personal Project' companyName='Tea Cozy Design' workDesc='Html, CSS' redirect='https://tea-cozy-2fba6.web.app/' />
     </div>
   )
