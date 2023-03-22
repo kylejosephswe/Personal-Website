182871be540160d483ada1334e02270ccc986baa
Kyle Joseph <kyle@Kyles-MacBook-Air.local>
2023-03-22 02:47:47 -0700

    Removed Tea Cozy

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,7 +7,6 @@ import Keshi from '.././images/keshiImage.jpg'
 import NikeLogo from '.././images/Nike3.webp'
 import animeImage from '.././images/animeImage1.jpg'
 import pokemon from '.././images/pokemon.jpg'
-import teaImage from '.././images/teaImage.jpg'
 
 function Projects() {
   return (
@@ -18,7 +17,6 @@ function Projects() {
       <Work image={pokemon} altImage='Pokedex' jobTitle='Personal Project' companyName='National Pokédex' workDesc='React, PokéAPI, Context API, Firebase, CSS' redirect='https://national-pokedex.web.app/' />
       <Work image={Keshi} altImage='Keshi' jobTitle='Personal Project' companyName='Keshi Music Bracket' workDesc='TypeScript, Html, Firebase, CSS' redirect='https://keshi-music-bracket.web.app/' />
       <Work image={animeImage} altImage='anime retriever' jobTitle='Personal Project' companyName='Anime Retriever' workDesc='React, Fetch API, Firebase, CSS' redirect='https://anime-retriever.web.app/' />
-      <Work image={teaImage} altImage='tea cozy' jobTitle='Personal Project' companyName='Tea Cozy Design' workDesc='Html, CSS' redirect='https://tea-cozy-2fba6.web.app/' />
     </div>
   )
 }
