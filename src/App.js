f69b09438956961fecdcd7747ff5c3e957c6f254
kylejoseph46 <kylejosephswe@gmail.com>
2023-08-24 15:10:12 -0700

    Added a date and skills section for the work page. And also added my education.

--- src/App.js
+++ src/App.js
@@ -14,6 +14,8 @@ import ScrollToTop from "./components/ScrollToTop";
 //Local Images.
 import boomer from "./images/Boomer.gif";
 import boomer2 from "./images/Boomer2.gif";
+import boomer3 from "./images/Boomer3.gif";
+import boomer4 from "./images/Boomer4.gif";
 
 //I use lazy loading the Footer instead of importing it regularly to improve performance.
 //import Footer from './Footer'
@@ -37,7 +39,9 @@ function App() {
                   altImage="oldComputer2"
                   jobTitle="Software Engineer Intern"
                   companyName="Siemens EDA"
-                  workDesc="I worked on the Parasitic Extraction (PEX) Team as a Software Engineer Intern to make a major accuracy analyzer project for xRC over the span of 6 months"
+                  companyDate="Apr 2023 to Sep 2023"
+                  workDesc="I worked on the Parasitic Extraction (PEX) Team as a Software Engineer Intern to make a major accuracy analyzer project for xACT3D over the span of 6 months"
+                  skills="Python, Bash, C++, Linux, Git, Jira"
                   redirect="https://eda.sw.siemens.com/en-US/ic/calibre-design/circuit-verification/xact-3d/"
                 />
                 <Work
@@ -45,8 +49,30 @@ function App() {
                   altImage="oldComputer1"
                   jobTitle="Frontend Engineer"
                   companyName="SunnySide"
+                  companyDate="Jan 2023 to Jul 2023"
                   workDesc="Contributed to the SunnySide app in preparation for the official launch by implementing in-app screens, and features needed for a seamless user experience"
+                  skills="JavaScript, TypeScript, React, React Native, CSS, Git"
                   redirect="https://www.sunnyside.care/"
+                />
+                <Work
+                  image={boomer3}
+                  altImage="Old computer"
+                  jobTitle="IT Consultant"
+                  companyName="PSU Office of Academic Innovation"
+                  companyDate="Oct 2021 to Jan 2023"
+                  workDesc="Primarily worked on migrating course content from the D2L learning space to Canvas and working on audits"
+                  skills="Salesforce, Notion, Html, CSS"
+                  redirect="https://www.pdx.edu/academic-innovation/"
+                />
+                <Work
+                  image={boomer4}
+                  altImage="Object Spinning"
+                  jobTitle="Computer Science Major"
+                  companyName="Portland State University"
+                  companyDate="Sep 2019 to Dec 2024"
+                  workDesc="Concentration in Software Engineering"
+                  skills="Web Development, Operating Systems, Systems Programming, Data Structures & Algorithms, Software Engineering"
+                  redirect="https://www.pdx.edu/"
                 />
                 <Contact />
                 <Suspense
--- src/components/Work.js
+++ src/components/Work.js
@@ -1,22 +1,56 @@
-function Work({ image, altImage, jobTitle, companyName, workDesc, redirect}) {
+function Work({
+  image,
+  altImage,
+  jobTitle,
+  companyName,
+  companyDate,
+  workDesc,
+  skills,
+  redirect,
+}) {
   return (
     <div>
       <div className="work-flex-container">
         <div className="work-image-text-row">
           <div>
-             {redirect ? <a className="project-links" href={redirect} target="_blank" rel="noreferrer" > <img className="work-img-design" width="500" height="460" src={image} alt={altImage} /> </a> : <img className="work-img-design" src={image} alt={altImage} />}
+            {redirect ? (
+              <a
+                className="project-links"
+                href={redirect}
+                target="_blank"
+                rel="noreferrer"
+              >
+                {" "}
+                <img
+                  className="work-img-design"
+                  width="500"
+                  height="460"
+                  src={image}
+                  alt={altImage}
+                />{" "}
+              </a>
+            ) : (
+              <img className="work-img-design" src={image} alt={altImage} />
+            )}
           </div>
           <div className="work-text-column">
             <h3 className="job-title">{jobTitle}</h3>
-            <a className="a-link" href={redirect} target="_blank" rel="noreferrer">
-            <h2 className="company-name">{companyName}</h2>
+            <a
+              className="a-link"
+              href={redirect}
+              target="_blank"
+              rel="noreferrer"
+            >
+              <h2 className="company-name">{companyName}</h2>
             </a>
+            <h2 className="company-date">{companyDate}</h2>
             <p className="work-desc">{workDesc}</p>
+            <p className="skills">{skills ? skills : null}</p>
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Work
+export default Work;
