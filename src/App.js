3307642994119ffbfe71ace7b87d4b02e22dc048
Kyle Joseph <kyle@Kyles-MacBook-Air.local>
2023-10-21 21:35:12 -0700

    Changed the wording on the work section.

--- src/App.js
+++ src/App.js
@@ -51,7 +51,7 @@ function App() {
                   jobTitle="Software Engineer Intern"
                   companyName="Siemens EDA"
                   companyDate="Apr 2023 to Sep 2023"
-                  workDesc="I worked on the Parasitic Extraction (PEX) Team as a Software Engineer Intern to make a major accuracy analyzer project for xACT 3D over the span of 6 months"
+                  workDesc="I worked on the Parasitic Extraction (PEX) Team as a Software Engineer intern to make a major accuracy analyzer project for Siemens Calibre xRC over the span of 6 months"
                   skills="Python, Bash, C++, Linux, Git, Jira, Agile, MS Office"
                   redirect="https://eda.sw.siemens.com/en-US/ic/calibre-design/circuit-verification/xact-3d/"
                 />
@@ -71,7 +71,7 @@ function App() {
                   jobTitle="IT Consultant"
                   companyName="PSU Office of Academic Innovation"
                   companyDate="Oct 2021 to Jan 2023"
-                  workDesc="Primarily worked on migrating course content from the D2L learning space to Canvas and working on audits"
+                  workDesc="Worked on migrating my university's course content from the D2L learning space to the Canvas platform"
                   skills="Salesforce, Notion, Html, CSS"
                   redirect="https://www.pdx.edu/academic-innovation/"
                 />
