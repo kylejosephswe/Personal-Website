8e5f620b339fdece84594e37161e84d37c068f90
Kyle Joseph <kyle@Kyles-MacBook-Air.local>
2023-11-08 00:04:22 -0800

    Changed URL for Meloverna.

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -13,7 +13,7 @@ function Projects() {
     <div>
       <Navbar />
       <h1 className="projects-header">Projects</h1>
-      <Work image={animeImage} altImage='Meloverna' jobTitle='Personal Project' companyName='Meloverna' workDesc='SQL, Kotlin, Python, Flask, JavaScript, React.js, Jest, JUnit, CSS, Git' redirect='https://anime-retriever.web.app/' />
+      <Work image={animeImage} altImage='Meloverna' jobTitle='Personal Project' companyName='Meloverna' workDesc='SQL, Kotlin, Python, Flask, JavaScript, React.js, Jest, JUnit, CSS, Git' redirect='https://meloverna-d69d4.web.app/' />
       <Work image={NikeLogo} altImage='Nike Sneaker Gallery' jobTitle='Personal Project' companyName='Nike Sneaker Gallery' workDesc='TypeScript, React, Router, Context API, Firebase, CSS' redirect='https://sneaker-gallery.web.app/' />
       <Work image={pokemon} altImage='Pokedex' jobTitle='Personal Project' companyName='National Pokédex' workDesc='React, PokéAPI, Context API, Firebase, CSS' redirect='https://national-pokedex.web.app/' />
       <Work image={Keshi} altImage='Keshi' jobTitle='Personal Project' companyName='Keshi Music Bracket' workDesc='TypeScript, Html, Firebase, CSS' redirect='https://keshi-music-bracket.web.app/' />
